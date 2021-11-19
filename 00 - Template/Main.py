f54b6f1e6115d1742c4c218a4c0cf7c4ee62b0a1
vrzv998 <32388502+vrzv998@users.noreply.github.com>
2021-11-19 10:24:05 +0700

    Add files via upload

---
+++ 00 - Template/Main.py
@@ -0,0 +1,19 @@
+import time
+start_time = time.time()
+print("Hello")
+print("World")
+
+#ini adalah comment
+
+a = 10 #assignment
+
+"""Commment
+multi line"""
+
+print(a)
+
+print(time.time()-start_time, "detik")
+#kita bisa mengcompile python
+#ke byte code
+# cara mengcompile, buka terminal dan tuliskan
+# python -m py_compile Main.py
---
+++ 03 - Casting Tipe Data/Main.py
@@ -0,0 +1,17 @@
+# casting = merubah dari satu tipe ke tipe lain
+# tipe data = int, float, str, bool
+
+## INTEGER
+data_int = 9;
+print("data = ", data_int, ",type =",type(data_int))
+
+# FLOAT
+data_float = float(data_int)
+print("data = ", data_float, ",type =",type(data_float))
+
+
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data = ", data_str, ",type =",type(data_str))
+print("data = ", data_bool, ",type =",type(data_bool))
+
